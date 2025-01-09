909c7a158214a135232c3cf0dca94ee6a269ebec
cozym <ddongyee99@daum.net>
2025-01-09 14:31:31 +0900

    feat: UserController 생성

---
+++ src/main/java/com/example/Insideout/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.example.Insideout.controller;
+
+import com.example.Insideout.dto.UserDto;
+import com.example.Insideout.entity.User;
+import com.example.Insideout.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<User> registerUser(@RequestBody UserDto userDto) {
+        try {
+            User savedUser = userService.registerUser(userDto);
+            return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+        }
+    }
+}
+
