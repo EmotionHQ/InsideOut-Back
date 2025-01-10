488f5de0220448cc78628f5195f6772a4f2127f6
Logan <jjk1491@gamil.com>
2025-01-10 13:45:52 +0900

    feat : 비밀번호 암호화 로직 구현

--- src/main/java/com/example/Insideout/service/UserService.java
+++ src/main/java/com/example/Insideout/service/UserService.java
@@ -4,22 +4,25 @@ import com.example.Insideout.dto.UserDto;
 import com.example.Insideout.entity.User;
 import com.example.Insideout.repository.UserRepository;
 import java.time.LocalDateTime;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public User registerUser(UserDto userDto) {
         User user = new User();
 
         user.setUserId(userDto.getUserId());
-        user.setPasswordHash(userDto.getPasswordHash());
+        user.setPasswordHash(passwordEncoder.encode(userDto.getPasswordHash()));
         user.setName(userDto.getName());
         user.setEmail(userDto.getEmail());
         user.setPhoneNumber(userDto.getPhoneNumber());
