062715c3ff7343628f8493829f9369be9a1e5b95
cozym <ddongyee99@daum.net>
2025-01-10 16:25:18 +0900

    feat: SecurityConfig 파일 수정(주석 수정)

--- src/main/java/com/example/Insideout/config/SecurityConfig.java
+++ src/main/java/com/example/Insideout/config/SecurityConfig.java
@@ -2,22 +2,47 @@ package com.example.Insideout.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
+    /**
+     * Spring Security 필터 체인 설정 CSRF 비활성화 JWT 필터 추가
+     */
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/**").permitAll() // 특정 경로 인증 없이 허용
-                        .anyRequest().authenticated() // 나머지 요청은 인증 필요
+                        .requestMatchers("/api/auth/login").permitAll()
+                        .requestMatchers("/api/**").permitAll()         // 특정 경로 인증 없이 허용
+                        .anyRequest().authenticated()
                 )
-                .httpBasic(httpBasic -> httpBasic.realmName("MyAppRealm"));
+                // 세션 관리 정책: STATELESS
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .httpBasic(httpBasic -> httpBasic.realmName("MyAppRealm"))
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
