fce8aa057c0134e030d90711dd68fdb776d38b28
hyeyun hwang <hhyeyoun@naver.com>
2025-01-15 16:36:57 +0900

    feat: 비밀번호 정규표현식 변경

--- src/main/java/com/example/Insideout/dto/UserDto.java
+++ src/main/java/com/example/Insideout/dto/UserDto.java
@@ -14,8 +14,8 @@ public class UserDto {
     private String userId;
 
     @NotBlank
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z\\d!@#$%^&*(),.?\":{}|<>]{8,32}$",
-            message = "비밀번호는 대소문자, 숫자, 특수문자를 각각 1개 이상 포함하고 8~32자여야 합니다.")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*\\d)(?=.*[!@#$%^&*(),.?\":{}|<>])[A-Za-z\\d!@#$%^&*(),.?\":{}|<>]{8,32}$",
+            message = "비밀번호는 숫자, 특수문자를 각각 1개 이상 포함하고 8~32자여야 합니다.")
     private String passwordHash;
 
     private String name;
