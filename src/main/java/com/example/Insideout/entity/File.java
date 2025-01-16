5cfbd4f9d2384bbdbd51d2abbaec0a0e96109383
Logan <jjk1491@gamil.com>
2025-01-16 17:15:03 +0900

    feat: File Entity Columns 추가

--- src/main/java/com/example/Insideout/entity/File.java
+++ src/main/java/com/example/Insideout/entity/File.java
@@ -2,9 +2,12 @@ package com.example.Insideout.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import lombok.Getter;
@@ -21,8 +24,15 @@ public class File {
     @Column(name = "file_id", nullable = false)
     private Long fileId;
 
-    @Column(name = "inquiry_id", nullable = false)
-    private Long inquiryId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "inquiry_id", nullable = false)
+    private Board board;
+
+    @Column(name = "original_name", nullable = false)
+    private String originalName;
+
+    @Column(name = "save_name", nullable = false)
+    private String saveName;
 
     @Column(name = "file_path", nullable = false)
     private String filePath;
