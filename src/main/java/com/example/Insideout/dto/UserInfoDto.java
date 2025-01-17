d998702a6af25f3be47e5083f7680c863acdcf33
eoeosb <eoeosb@gmail.com>
2025-01-17 10:33:35 +0900

    Feat: User Info DTO 추가

---
+++ src/main/java/com/example/Insideout/dto/UserInfoDto.java
@@ -0,0 +1,16 @@
+package com.example.Insideout.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserInfoDto {
+    private String userId;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String department;
+    private String role;
+    private String deptCode;
+} 
