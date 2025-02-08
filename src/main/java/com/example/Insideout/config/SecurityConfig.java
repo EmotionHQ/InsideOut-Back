0268313edd5a4982500d50017230c8ead4ebc32b
eoeosb <eoeosb@gmail.com>
2025-02-08 18:42:28 +0900

    Chore: CORS Front 주소 추가

--- src/main/java/com/example/Insideout/config/SecurityConfig.java
+++ src/main/java/com/example/Insideout/config/SecurityConfig.java
@@ -72,7 +72,9 @@ public class SecurityConfig {
                 "https://insideout-front.netlify.app",
                 "http://localhost:8000",  // FastAPI 서버 URL
                 "https://insideout-ai-production.up.railway.app",
-                "https://insideout-front-production.up.railway.app"
+                "https://insideout-front-production.up.railway.app",
+                "https://insideout-front.azurewebsites.net",
+                "https://emotionhq.com"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
